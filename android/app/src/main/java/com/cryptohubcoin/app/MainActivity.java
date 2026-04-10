184f5a909916a649087559cd5e8e5798357279e7
cryptohubcoin <142168789+cryptohubcoin@users.noreply.github.com>
2026-04-10 22:38:55 +0200

    Create MainActivity.java

---
+++ android/app/src/main/java/com/cryptohubcoin/app/MainActivity.java
@@ -0,0 +1,37 @@
+package com.cryptohubcoin.app;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    
+    private WebView webView;
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        webView = findViewById(R.id.webview);
+        
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+        settings.setCacheMode(WebSettings.LOAD_DEFAULT);
+        
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://www.cryptohubcoin.com/");
+    }
+    
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
